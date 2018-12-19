50960b93163722ac62110bb85d7cbff22eb60f9a
f17007588 <yann.forner@etu.univ-amu.fr>
2018-12-19 13:32:48 +0100

    Composite

--- src/Composite/Main.java
+++ src/Composite/Main.java
@@ -2,6 +2,17 @@ package Composite;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        Branche branche = new Branche();
+        Branche branche1 = new Branche();
+        Branche branche2 = new Branche();
+
+        branche.ajouterArbre(branche1);
+        branche.ajouterArbre(branche2);
+
+        branche1.ajouterArbre(new Feuille());
+        branche1.ajouterArbre(new Feuille());
+
+        branche2.ajouterArbre(new Feuille());
+        branche.identifier();
     }
 }
